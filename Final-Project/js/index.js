b382e44570f6d3bede73989323e4f21b1b701a2d
CodergirlVS <vaidehis1214@gmail.com>
2021-06-01 16:26:23 -0700

    Added Skills

--- Final-Project/js/index.js
+++ Final-Project/js/index.js
@@ -4,17 +4,33 @@ const footer = document.querySelector('footer');
 const copyright = document.createElement('p')
 copyright.innerHTML = `&copy; Vaidehi ${thisYear}`;
 footer.appendChild(copyright);
+const skillsSection = document.getElementById('skills');
 
-const skills = ['JavaScript', 'Agile', 'Accounts', 'Communication']
+const techSkills = ['JavaScript', 'HTML/CSS', 'Microsoft Office', 'Visio', 'Basic SQL', 'Basic Tableau'];
+const techSkillsList = skillsSection.querySelector('ul#technicalSkills');
 
-const skillsSection = document.getElementById('skills')
+const busnSkills = ['Process GAP Analysis', 'Requirement Gathering and Analysis', 'USE Case', 'UAT Testing',
+                     'Performance Analysis and Enhancement Solution', 'KPI and Dashboard Scorecard'];
+const busnSkillsList = skillsSection.querySelector('ul#businessSkills');
 
-const skillsList = skillsSection.querySelector('ul')
+const personalSkills = ['Communication', 'leadership', 'Team Work', 'Prioritization', 'Organization skills', 'Focused and Detail Oriented'];
+const personalSkillsList = skillsSection.querySelector('ul#personalSkills');
 
-for(i = 0; i < skills.length; i++) {
-    const skill = document.createElement('li');
-    skill.innerText = skills[i];
-    skillsList.appendChild(skill)
+for(i = 0; i < techSkills.length; i++) {
+    const techSkill = document.createElement('li');
+    techSkill.innerText = techSkills[i];
+    techSkillsList.appendChild(techSkill);
 }
 
-console.log(skillsList)
+for(i = 0; i < busnSkills.length; i++) {
+    const busnSkill = document.createElement('li');
+    busnSkill.innerHTML = busnSkills[i];
+    busnSkillsList.appendChild(busnSkill);
+}
+
+ for(i = 0; i < personalSkills.length; i++) {
+    const personalSkill = document.createElement('li');
+    personalSkill.textContent = personalSkills[i];
+    personalSkillsList.appendChild(personalSkill);
+ }
+
