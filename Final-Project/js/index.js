02a2f8ee81c4c236a4a9cd5c3f8c62445e12621b
CodergirlVS <vaidehis1214@gmail.com>
2021-06-02 00:43:13 -0700

    Added eventListeners

--- Final-Project/js/index.js
+++ Final-Project/js/index.js
@@ -16,21 +16,60 @@ const busnSkillsList = skillsSection.querySelector('ul#businessSkills');
 const personalSkills = ['Communication', 'leadership', 'Team Work', 'Prioritization', 'Organization skills', 'Focused and Detail Oriented'];
 const personalSkillsList = skillsSection.querySelector('ul#personalSkills');
 
-for(i = 0; i < techSkills.length; i++) {
+const profileName = document.getElementsByTagName('h1')[0];
+
+profileName.addEventListener('mouseover', () => {
+    profileName.textContent = profileName.innerText.toUpperCase();
+});
+
+profileName.addEventListener('mouseout', () => {
+    profileName.textContent = profileName.textContent.toLowerCase();
+});
+
+skillsSection.addEventListener('mouseover', (event, event1) => {
+    if(event.target.tagName == 'LI') {
+        event.target.textContent = event.target.textContent.toUpperCase()
+      }
+});
+skillsSection.addEventListener('mouseout', (event) => {
+    if(event.target.tagName == 'LI') {
+        event.target.textContent = event.target.textContent.toLowerCase();
+      }
+});
+
+for(let i = 0; i < techSkills.length; i++) {
     const techSkill = document.createElement('li');
     techSkill.innerText = techSkills[i];
+    techSkill.addEventListener('mouseover', () => {
+        techSkill.style.color = 'red';
+    });
+    techSkill.addEventListener('mouseout', () => {
+        techSkill.style.color = 'black';
+    });
     techSkillsList.appendChild(techSkill);
-}
+};
 
-for(i = 0; i < busnSkills.length; i++) {
+for(let i = 0; i < busnSkills.length; i++) {
     const busnSkill = document.createElement('li');
     busnSkill.innerHTML = busnSkills[i];
+    busnSkill.addEventListener('mouseover', () => {
+        busnSkill.style.color = 'red';
+    });
+    busnSkill.addEventListener('mouseout', () => {
+        busnSkill.style.color = 'black';
+    });
     busnSkillsList.appendChild(busnSkill);
-}
+};
 
- for(i = 0; i < personalSkills.length; i++) {
+ for(let i = 0; i < personalSkills.length; i++) {
     const personalSkill = document.createElement('li');
     personalSkill.textContent = personalSkills[i];
+    personalSkill.addEventListener('mouseover', () => {
+        personalSkill.style.color = 'red';
+    });
+    personalSkill.addEventListener('mouseout', () => {
+        personalSkill.style.color = 'black';
+    });
     personalSkillsList.appendChild(personalSkill);
- }
+ };
 
