86af87ebd9f228350e4bd52a21a4f5618d6b8d9b
CodergirlVS <vaidehis1214@gmail.com>
2021-07-20 16:43:21 -0700

    Final project ready for submission

--- Final-Project/js/index.js
+++ Final-Project/js/index.js
@@ -5,72 +5,115 @@ const thisYear = today.getFullYear();
 const footer = document.querySelector("footer");
 const copyright = document.getElementById("footerP");
 copyright.innerHTML = `&copy; Vaidehi ${thisYear}`;
-footer.appendChild(copyright);
 
-const profileName = document.getElementById("profileName");
+// const profileName = document.getElementById("profileName");
 
+const skillsArray = [
+  {
+    name: "JavaScript",
+    category: "Tech",
+  },
+  {
+    name: "HTML/CSS",
+    category: "Tech",
+  },
+  {
+    name: "Microsoft Office",
+    category: "Tech",
+  },
+  {
+    name: "Visio",
+    category: "Tech",
+  },
+  {
+    name: "Basic SQL",
+    category: "Tech",
+  },
+  {
+    name: "Basic Tableau",
+    category: "Tech",
+  },
+  {
+    name: "Process GAP Analysis",
+    category: "Business",
+  },
+  {
+    name: "Requirement Gathering and Analysis",
+    category: "Business",
+  },
+  {
+    name: "USE Case",
+    category: "Business",
+  },
+  {
+    name: "UAT Testing",
+    category: "Business",
+  },
+  {
+    name: "Performance Analysis and Enhancement Solution",
+    category: "Business",
+  },
+  {
+    name: "KPI and Dashboard Scorecard",
+    category: "Business",
+  },
+  {
+    name: "Communication",
+    category: "Personal",
+  },
+  {
+    name: "Leadership",
+    category: "Personal",
+  },
+  {
+    name: "Prioritization",
+    category: "Personal",
+  },
+  {
+    name: "Team Work",
+    category: "Personal",
+  },
+  {
+    name: "Organization skills",
+    category: "Personal",
+  },
+  {
+    name: "Focused and Detail Oriented",
+    category: "Personal",
+  },
+];
 const skillsSection = document.getElementById("skills");
+const techSkillsList = document.getElementById("technicalSkills");
+const busnSkillsList = document.querySelector("#businessSkills");
+const personalSkillsList = document.querySelector("#personalSkills");
+
+for (let i = 0; i < skillsArray.length; i++) {
+  const skillsLi = document.createElement("li");
+  skillsLi.setAttribute("data-id", i);
+  skillsLi.innerText = skillsArray[i].name;
+  if (skillsArray[i].category === "Tech") {
+    techSkillsList.appendChild(skillsLi);
+  } else if (skillsArray[i].category === "Business") {
+    busnSkillsList.appendChild(skillsLi);
+  } else if (skillsArray[i].category === "Personal") {
+    personalSkillsList.appendChild(skillsLi);
+  }
+}
 
 skillsSection.addEventListener("mouseover", (event) => {
   if (event.target.tagName == "LI") {
     event.target.textContent = event.target.textContent.toUpperCase();
-    event.target.style.color = "red";
+    event.target.style.color = "blue";
   }
 });
 skillsSection.addEventListener("mouseout", (event) => {
   if (event.target.tagName == "LI") {
-    event.target.textContent = event.target.textContent.toLowerCase();
+    const index = event.target.getAttribute("data-id");
+    console.log(index);
+    event.target.textContent = skillsArray[index].name;
     event.target.style.color = "black";
   }
 });
-
-const techSkills = [
-  "JavaScript",
-  "HTML/CSS",
-  "Microsoft Office",
-  "Visio",
-  "Basic SQL",
-  "Basic Tableau",
-];
-const techSkillsList = skillsSection.querySelector("ul#technicalSkills");
-
-const busnSkills = [
-  "Process GAP Analysis",
-  "Requirement Gathering and Analysis",
-  "USE Case",
-  "UAT Testing",
-  "Performance Analysis and Enhancement Solution",
-  "KPI and Dashboard Scorecard",
-];
-const busnSkillsList = skillsSection.querySelector("ul#businessSkills");
-
-const personalSkills = [
-  "Communication",
-  "leadership",
-  "Team Work",
-  "Prioritization",
-  "Organization skills",
-  "Focused and Detail Oriented",
-];
-const personalSkillsList = skillsSection.querySelector("ul#personalSkills");
-
-for (let i = 0; i < techSkills.length; i++) {
-  const techSkill = document.createElement("li");
-  techSkill.innerText = techSkills[i];
-  techSkillsList.appendChild(techSkill);
-}
-
-for (let i = 0; i < busnSkills.length; i++) {
-  const busnSkill = document.createElement("li");
-  busnSkill.innerHTML = busnSkills[i];
-  busnSkillsList.appendChild(busnSkill);
-}
-
-for (let i = 0; i < personalSkills.length; i++) {
-  const personalSkill = document.createElement("li");
-  personalSkill.textContent = personalSkills[i];
-  personalSkillsList.appendChild(personalSkill);
-}
 
 //*****************************************************************Leasson-4-3****************************************************
 //                        ************* CODE REMOVES THE EDIT BUTTON AND CREATES A SAVE BUTTON **********************
@@ -96,27 +139,27 @@ const editFunction = function (e) {
   const nameInput = document.createElement("input");
   nameInput.type = "text";
   nameInput.value = name.textContent;
-  nameInput.className = 'userInput';
+  nameInput.className = "userInput";
 
-  const emailInput = document.createElement('input');
-  emailInput.type = 'email'
+  const emailInput = document.createElement("input");
+  emailInput.type = "email";
   emailInput.value = email.href.slice(7);
-  emailInput.className = 'userInput';
+  emailInput.className = "userInput";
 
   const messageInput = document.createElement("textarea");
   messageInput.type = "text";
-  messageInput.id = 'userMessageInput';
+  messageInput.id = "userMessageInput";
   messageInput.value = message.textContent.slice(8);
-  
+
   const messageDate = new Date();
-  const span = document.createElement('span');
+  const span = document.createElement("span");
   span.innerText = `${messageDate.toLocaleString()} `;
 
-  while (li.children.length > 0){
+  while (li.children.length > 0) {
     li.removeChild(li.firstElementChild);
   }
 
-  const div = document.createElement('div');
+  const div = document.createElement("div");
   div.appendChild(span);
   div.appendChild(nameInput);
   div.appendChild(emailInput);
@@ -125,8 +168,8 @@ const editFunction = function (e) {
   li.appendChild(div);
   li.appendChild(createSaveButton());
   li.appendChild(createRemoveButton());
-  
-  e.target.addEventListener('click', saveFunction);
+
+  e.target.addEventListener("click", saveFunction);
 };
 
 const saveFunction = function (e) {
@@ -136,12 +179,11 @@ const saveFunction = function (e) {
   const email = li.firstElementChild.children[2].value;
   const message = li.firstElementChild.children[3].value;
 
-  while (li.children.length > 0){
+  while (li.children.length > 0) {
     li.removeChild(li.firstElementChild);
   }
-  
+
   populateLi(li, name, email, message);
-  
 };
 //Creating Save Button
 
@@ -149,7 +191,7 @@ function createSaveButton() {
   const saveButton = document.createElement("button");
   saveButton.textContent = "Save";
   saveButton.type = "Save";
-  saveButton.className = 'buttons';
+  saveButton.className = "buttons";
   saveButton.addEventListener("click", saveFunction);
   return saveButton;
 }
@@ -171,8 +213,8 @@ function createRemoveButton() {
   const removeButton = document.createElement("button");
   removeButton.textContent = "Remove";
   removeButton.type = "button";
-  removeButton.className = "buttons"
-  removeButton.style.backgroundColor = 'red'
+  removeButton.className = "buttons";
+  removeButton.style.backgroundColor = "red";
   removeButton.addEventListener("click", (event) => {
     const entry = event.target.parentNode;
     entry.remove();
@@ -183,23 +225,23 @@ function createRemoveButton() {
 
 function formLiText(fullName, email, message) {
   const messageDate = new Date();
-  const div = document.createElement('div');
-  const span1 = document.createElement('span');
+  const div = document.createElement("div");
+  const span1 = document.createElement("span");
   span1.innerText = `${messageDate.toLocaleString()} `;
-  const a = document.createElement('a');
-    a.href = `mailto:${email}`;
-    a.innerText = fullName;
-  const span = document.createElement('span');
-    span.innerText = ` wrote: ${message}`;
-  div.append(span1, a, span)
-    return div;
+  const a = document.createElement("a");
+  a.href = `mailto:${email}`;
+  a.innerText = fullName;
+  const span = document.createElement("span");
+  span.innerText = ` wrote: ${message}`;
+  div.append(span1, a, span);
+  return div;
 }
 
 // creating Li
 
-function createLi (fullName, email, message) {
+function createLi(fullName, email, message) {
   const newLi = document.createElement("li");
-  newLi.classList.add('newMessageList');
+  newLi.classList.add("newMessageList");
   return populateLi(newLi, fullName, email, message);
 }
 
@@ -226,39 +268,38 @@ messageForm.addEventListener("submit", (e) => {
 //*************************************Lessaon-6-2**************************************************
 
 fetch("https://api.github.com/users/CodergirlVS/repos")
-  .then(checkStatus)  
-  .then(response => response.json())
-  .then(data => onload(data)
-  )
-  .catch(error => {
-    console.log('Ooops!'+' '+ error)
-  })
+  .then(checkStatus)
+  .then((response) => response.json())
+  .then((data) => onload(data))
+  .catch((error) => {
+    console.log("Ooops!" + " " + error);
+  });
 
 function checkStatus(response) {
-  if(response.ok) {
+  if (response.ok) {
     return Promise.resolve(response);
-  }else {
-    return Promise.reject(new Error (response.statusText));
+  } else {
+    return Promise.reject(new Error(response.statusText));
   }
-};
+}
 
-function onload (data) {
-    const projectSection = document.getElementById('projects');
-    const projectList = projectSection.querySelector('ul');
-      for(i = 0; i < data.length; i++){
-        const project = document.createElement('li');
-        project.className = "repoList";
-        const atag = document.createElement('a');
-        atag.className = "projectLink";
-        atag.href = data[i].html_url;
-        atag.innerText = data[i].name;
-        const ptag = document.createElement('p'); 
-        ptag.innerText = data[i].created_at.slice(0,10) + "\n" + data[i].description;
-        const itag = document.createElement('i');
-        itag.className = "pin";
-        projectList.appendChild(project);
-        project.append(atag, ptag, itag);
-    }
-};
-
-
+function onload(data) {
+  const projectSection = document.getElementById("projects");
+  const projectList = projectSection.querySelector("ul");
+  for (i = 0; i < data.length; i++) {
+    const project = document.createElement("li");
+    project.className = "repoList";
+    const atag = document.createElement("a");
+    atag.className = "projectLink";
+    atag.setAttribute("target", "_blank");
+    atag.href = data[i].html_url;
+    atag.innerText = data[i].name;
+    const ptag = document.createElement("p");
+    ptag.innerText =
+      data[i].created_at.slice(0, 10) + "\n" + data[i].description;
+    const itag = document.createElement("i");
+    itag.className = "fa fa-thumb-tack pin";
+    projectList.appendChild(project);
+    project.append(atag, ptag, itag);
+  }
+}
